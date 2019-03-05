a077023f9c6ed70aeb268591cd0a5178f96bed79
Alexis W. Waisman <alexis.waisman@gmail.com>
2019-03-05 15:36:05 -0300

    modificado metodo gethoras por marcar

--- controlador.py
+++ controlador.py
@@ -7,12 +7,12 @@ from modelo import operacionSimple
 # TODO: Construir función que reste horas entre si de manera tal que se pueda calcular tiempo entre marcaciones.
 # TODO: construir función que permita la marcación en el momento que se la llama
 
-def getTiempo():
+def marcar():
     from datetime import datetime
     tiempo = datetime.now()
     return [tiempo.day,tiempo.month,tiempo.year,tiempo.hour,tiempo.minute]
 
-def diferenciaHoras(p_marca1,p_marca2): 
+def diferenciaHoras(p_marca1,p_marca2):
     from datetime import datetime
     marca1 = datetime.strptime("%s %s %s %s %s"%tuple(p_marca1),"%d %m %Y %H %M")
     marca2 = datetime.strptime("%s %s %s %s %s"%tuple(p_marca2),"%d %m %Y %H %M")
@@ -20,8 +20,10 @@ def diferenciaHoras(p_marca1,p_marca2):
     return lapso
 
 
+
+
 if __name__ == '__main__':
-    print((diferenciaHoras([5,3,2019,8,15],[5,3,2019,16,25])))
+    print(marcar())
 
 
 
