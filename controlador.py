1e9a8863d24c0a4c1f8002fd4f18f5e5d6793a22
Alexis W. Waisman <alexis.waisman@gmail.com>
2019-03-05 15:34:51 -0300

    actualizado diferenciaHoras, ahora toma los dias y calcula la diferencia en dias

--- controlador.py
+++ controlador.py
@@ -12,16 +12,16 @@ def getTiempo():
     tiempo = datetime.now()
     return [tiempo.day,tiempo.month,tiempo.year,tiempo.hour,tiempo.minute]
 
-def diferenciaHoras(p_marca1,p_marca2): # marca = [hora,minuto]
+def diferenciaHoras(p_marca1,p_marca2): 
     from datetime import datetime
-    marca1 = datetime.strptime("%s%s"%tuple(p_marca1),"%H%M")
-    marca2 = datetime.strptime("%s%s"%tuple(p_marca2),"%H%M")
+    marca1 = datetime.strptime("%s %s %s %s %s"%tuple(p_marca1),"%d %m %Y %H %M")
+    marca2 = datetime.strptime("%s %s %s %s %s"%tuple(p_marca2),"%d %m %Y %H %M")
     lapso = marca2 - marca1
     return lapso
 
 
 if __name__ == '__main__':
-    print(diferenciaHoras([8,15],[16,25]))
+    print((diferenciaHoras([5,3,2019,8,15],[5,3,2019,16,25])))
 
 
 
