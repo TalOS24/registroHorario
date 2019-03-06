c3c9f4448710526c62260231a5f812cf92906285
Alexis W. Waisman <alexis.waisman@gmail.com>
2019-03-06 13:46:29 -0300

    se procedera a alterar la tabla jornadas para que los dias,meses, y años sean unicos

--- controlador.py
+++ controlador.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from modelo import operacionSimple
+from modelo import *
 
 
-
-def marcar():
+def getTiempo():
     from datetime import datetime
     tiempo = datetime.now()
     return [tiempo.day,tiempo.month,tiempo.year,tiempo.hour,tiempo.minute]
@@ -17,11 +16,38 @@ def diferenciaHoras(p_marca1,p_marca2):
     lapso = marca2 - marca1
     return lapso
 
+def marcar():
+
+    # Revisa la marca del ultimo registro
+    inicioRotacion = 1
+    ultimoTipoMarca = seleccion(" SELECT ID FROM  tipoMarca ORDER BY ID DESC LIMIT 1")[0][0]
+    ultimaMarca = seleccion(" SELECT marca_FK FROM  Asistencias ORDER BY jornada_FK DESC LIMIT 1")[0][0]
+    marca = (ultimaMarca % ultimoTipoMarca) + inicioRotacion
+
+    # consigue el tiempo actual
+
+    temp = getTiempo()
+    fechaActual = temp[:3]
+    horaMinutoActual = temp[-2:]
+
+    # TODO: si no existe la jornada crearla
+        # comparar el tiempo obtenido con la maquina con el de la ultima jornada existente (si no existe se crea)
+    ultimaJornada = seleccion("SELECT dia,mes,anio FROM jornadas ORDER BY ID DESC LIMIT 1")[0]
+    ultimaJornada = (3,3,2019) # prueba
+    if fechaActual == list(ultimaJornada):
+        IDjor = seleccion("SELECT ID FROM jornadas WHERE DIA=%s AND MES=%s AND ANIO=%s"%ultimaJornada)[0][0]
+    else:
+        # se crea la jornada
+        IDjor = operacionSimple("A","jornadas"," 'dia', 'mes', 'anio' ", " %s, %s , %s "%tuple(fechaActual))
+        IDjor = seleccion("SELECT ID FROM jornadas WHERE DIA=%s AND MES=%s AND ANIO=%s" %tuple(fechaActual))[0][0]
+        print(IDjor)
 
 
-# TODO: cambiar la BD , solo registrar horas y minutos en Asistencias
+
 if __name__ == '__main__':
-    print(marcar())
+    conectar()
+    marcar()
+
 
 
 
