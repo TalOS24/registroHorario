9824b6cd0bedf1c9dd4d9b4e9700e4943f111e67
Alexis W. Waisman <alexis.waisman@gmail.com>
2019-03-13 16:16:25 -0300

    creada la funcion marcaciondiferida

--- controlador.py
+++ controlador.py
@@ -28,7 +28,43 @@ def diferenciaHoras(p_marca1,p_marca2):
     raise NameError('Error en la cantidad de parametros de fechas a sustraer (5 o 3) ningun otro valor.')
 
 
-def marcar():
+def marcacion():
+    try:
+        # Conseguir la marca . Revisando la marca del ultimo registro
+        inicioRotacion = 1
+        ultimoTipoMarca = seleccion(" SELECT ID FROM  tipoMarca ORDER BY ID DESC LIMIT 1")[0][0]
+        ultimaMarca = seleccion(" SELECT marca_FK FROM  Asistencias ORDER BY jornada_FK DESC, ID DESC  LIMIT 1")[0][0]
+        marca = (ultimaMarca % ultimoTipoMarca) + inicioRotacion
+
+        # consigue el tiempo actual
+        temp = getTiempo()
+        fechaActual = temp[:3]
+        horaMinutoActual = temp[-2:]
+
+        #Consigue la jornada
+        # comparar el tiempo obtenido con la maquina con el de la ultima jornada existente (si no existe se crea)
+        ultimaJornada = seleccion("SELECT dia,mes,anio FROM jornadas ORDER BY ID DESC LIMIT 1")[0]
+        if fechaActual == list(ultimaJornada):
+            IDjor = seleccion("SELECT ID FROM jornadas WHERE DIA=%s AND MES=%s AND ANIO=%s"%ultimaJornada)[0][0]
+        else:
+            # se analiza la jornada previa
+            jornadaPrevia = seleccion("SELECT ID from jornadas order by ID desc limit 1")[0][0]
+            calculoJornada(jornadaPrevia)
+            return None
+            # se crea la jornada
+            operacionSimple("A","jornadas"," 'dia', 'mes', 'anio', 'tipoJornada_FK' ", " %s, %s , %s, 1 "%tuple(fechaActual))
+            # La siguiente busqueda tambien puede ser por ultimo valor de jornadas
+            IDjor = seleccion("SELECT ID FROM jornadas WHERE DIA=%s AND MES=%s AND ANIO=%s" %tuple(fechaActual))[0][0]
+
+        #Marca asistencia
+        operacionSimple("A","Asistencias","  'jornada_FK', 'hora', 'minuto', 'marca_FK' "," %s, %s, %s, %s" %(IDjor,horaMinutoActual[0],horaMinutoActual[1],marca) )
+        return "Marcacion Exitosa!"
+
+    except Exception as e:
+        print(e)
+        return "Fallo en el proceso de Marcado :-("
+
+def marcacionDiferida():
     try:
         # Conseguir la marca . Revisando la marca del ultimo registro
         inicioRotacion = 1
